4d41a57d718957cee7fcc19ebb9b8b367dbc3010
Sebastien Siva <ssiva@ggc.edu>
2024-09-12 17:20:49 -0400

    Player V1 Ready

--- Prof_3DSnowman/Assets/Scripts/FP_Mover.cs
+++ Prof_3DSnowman/Assets/Scripts/FP_Mover.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class FP_Mover : MonoBehaviour
 {
-    public float speed;
+    Transform myTran;
 
+    public float speed;
+    public float rotSpeed;
+
+    private Vector3 myRot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTran = GetComponent<Transform>();
+        myRot = myTran.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move
+        //MOVE
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        Vector3 step = h * transform.right + v * transform.forward;
-        transform.position += (step * speed * Time.deltaTime);
+        Vector3 step = h * myTran.right + v * myTran.forward;
+        step.y = 0; // don't fly
+        myTran.position += (step * speed * Time.deltaTime);
+
+        //ROTATE
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+        Vector3 rotStep = mx * Vector3.up + my * Vector3.right;
+        myRot += (rotStep * rotSpeed * Time.deltaTime);
+        if(myRot.x > 30) {myRot.x = 30;}
+        if(myRot.x < -30) {myRot.x = -30;}
+        myTran.eulerAngles = myRot;
+
     }
 }
